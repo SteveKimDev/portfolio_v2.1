e096aea3a43e65c3bb8a59c6ad0e6926ad4304d6
stevekim72 <steve.kim72@yahoo.com>
2023-09-27 09:44:42 -0700

    layout + page +SEO setup

---
+++ src/layout/layout.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+// import Header from './header';
+// import Footer from './footer';
+
+import './styles/layout.css';
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      {/* <Header /> */}
+      <main>{children}</main>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default Layout;
